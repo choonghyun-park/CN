dccc52fe167faae58e4851688310be9633c7f636
Choonghyun_Park <choonghyun.park10@gmail.com>
2023-04-01 16:41:28 +0900

    update

--- PA1/server.py
+++ PA1/server.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8 -*-
 
 import socket
+import json
 
 # 통신 정보 설정
 IP = ''
@@ -12,7 +13,7 @@ ADDR = (IP, PORT)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     server_socket.bind(ADDR)  # 주소 바인딩
     server_socket.listen()  # 클라이언트의 요청을 받을 준비
-    # 무한루프 진입
+    
     while True:
         server_socket.settimeout(1)
         try:
@@ -21,6 +22,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
             continue
         server_socket.settimeout(None)
         msg = client_socket.recv(SIZE)  # 클라이언트가 보낸 메시지 반환
-        print("[{}] message : {}".format(client_addr,msg))  # 클라이언트가 보낸 메시지 출력
+        # msg -> json : loads()
+        json_msg = json.loads(msg.decode())
+        print("[client_addr : {}]".format(client_addr))
+        print("raw message : {}".format(msg))  # 클라이언트가 보낸 메시지 출력
+        print("json message : {}\n".format(json_msg))  # 클라이언트가 보낸 메시지 출력
         client_socket.sendall("welcome!".encode())  # 클라이언트에게 응답
         client_socket.close()  # 클라이언트 소켓 종료
---
+++ PA1/server_wo_template.py
@@ -0,0 +1,75 @@
+import json
+import wsgiref.simple_server
+import urllib.parse
+
+forms_data = []  # all submissions
+
+def application(environ, start_response):
+    # requested path
+    path = environ["PATH_INFO"]
+    # requested method
+    method = environ["REQUEST_METHOD"]
+    
+    # content type of response
+    content_type = "text/html"
+
+    if path == "/":
+        if method == "POST":
+            # getting wsgi.input obj
+            input_obj = environ["wsgi.input"]
+            # length of body
+            input_length = int(environ["CONTENT_LENGTH"])
+            # getting body of wsgi.input obj
+            # decoding to string
+            body = input_obj.read(input_length).decode()
+
+            # parsing body of form
+            data = urllib.parse.parse_qs(body, keep_blank_values=True)
+            # data of body in format
+            req = {
+                "name" : data["name"][0],
+                "email" : data["email"][0],
+                "type" : data["f-type"][0],
+                "content" : data["feedback"][0]
+            }
+            # adding to submission
+            forms_data.append(req)
+            
+            response = b"Your feedback submitted successfully."
+            status = "200 OK"
+        else:
+            # reading html file
+            with open("feedback.html","r") as f:
+                response = f.read().encode()
+            status = "200 OK"
+
+    elif path == "/forms":
+        # if /forms path
+        # converting to JSON data
+        response = json.dumps(forms_data).encode()
+        status = "200 OK"
+        # changing content-type
+        content_type = "application/json"
+
+    else:
+        # 404 - path not found
+        response = b"<h1>Not found</h1><p>Entered path not found</p>"
+        status = "404 Not Found"
+
+    # response headers
+    headers = [
+        ("Content-Type",content_type),
+        ("Content-Length",str(len(response)))
+    ]
+
+    start_response(status, headers)
+    return [response]
+    
+
+if __name__ == "__main__":
+    w_s = wsgiref.simple_server.make_server(
+        host="localhost",
+        port=8021,
+        app=application
+    )
+    w_s.serve_forever()
