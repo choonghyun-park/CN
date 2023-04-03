2c55797fe5f458a0a79cdc971959bce8f1c005d5
Choonghyun_Park <choonghyun.park10@gmail.com>
2023-04-03 19:05:28 +0900

    Q3-6 clear

--- PA1/server.py
+++ PA1/server.py
@@ -9,12 +9,14 @@ PORT = 1398
 SIZE = 1024
 ADDR = (IP, PORT)
 
-def hi(client_socket):
+users = {}
+
+def get_hi(client_socket):
     response = b'HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 16\r\nAccess-Control-Allow-Origin: *\r\n\r\n{"message":"hi"}'
     client_socket.send(response)
     print("{} was sent!".format(response))
 
-def echo(client_socket,msg_body_dict):
+def post_echo(client_socket,msg_body_dict):
     try:
         m = msg_body_dict["message"]
     except:
@@ -42,6 +44,70 @@ def echo(client_socket,msg_body_dict):
     
     client_socket.send(response)
     print("{} was sent!".format(response))
+
+def post_user(client_socket,msg_body_dict):
+    try:
+        id = msg_body_dict["id"]
+        name = msg_body_dict["name"]
+        gender = msg_body_dict["gender"]
+    except KeyError:
+        # 400
+        response = b'HTTP/1.1 400 Bad Request\r\nContent-Type: application/json\r\nContent-Length: 0\r\nAccess-Control-Allow-Origin: *\r\n\r\n'
+        client_socket.send(response)
+        return
+    # id가 존재하지 않는 경우등록
+    if id not in users.keys():
+        users[id]=(name,gender)
+        # 201
+        response = b'HTTP/1.1 201 Created\r\nContent-Type: application/json\r\nContent-Length: 0\r\nAccess-Control-Allow-Origin: *\r\n\r\n'
+
+    # 기존에 등록된 id : 409
+    else:
+        response = b'HTTP/1.1 409 Registered id\r\nContent-Type: application/json\r\nContent-Length: 0\r\nAccess-Control-Allow-Origin: *\r\n\r\n'
+    client_socket.send(response)
+
+def get_user(client_socket,query):
+    id = query[1]
+    assert id in users
+    # try:
+    #     user_info = users[id]
+    # except KeyError:
+    #     # 등록되지 않은 id : 404
+    #     response = b'HTTP/1.1 404 Not Found\r\nContent-Type: application/json\r\nContent-Length: 0\r\nAccess-Control-Allow-Origin: *\r\n\r\n'
+    #     return
+    
+    user_info = users[id]
+    name = user_info[0]
+    gender = user_info[1]
+
+    # make body
+    body = {"id":id,"name":name,"gender":gender}
+    body = json.dumps(body)
+    leng = len(body)
+
+    # make header
+    headers = []
+    headers.append('HTTP/1.1 200 OK')
+    headers.append('Content-Type: application/json')
+    headers.append('Content-Length: {}'.format(leng))
+    headers.append('Access-Control-Allow-Origin: *')
+    header = ''
+    for h in headers:
+        header += h
+        header += '\r\n'
+    header += '\r\n'
+
+    response = header + body
+    response = bytes(response, encoding='utf-8')
+    
+    client_socket.send(response)
+    print("{} was sent!".format(response))
+
+
+def page_not_found(client_socket):
+    response = b'HTTP/1.1 404 Not Found\r\nContent-Type: application/json\r\nContent-Length: 0\r\nAccess-Control-Allow-Origin: *\r\n\r\n'
+
+    client_socket.send(response)
 
 # 서버 소켓 설정
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
@@ -97,10 +163,28 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         except:
             msg_body_dict = {}
 
-        if method=='GET' and parmalink=='/hi':
-            hi(client_socket)
-        elif method=='POST' and parmalink=='/echo':
-            echo(client_socket,msg_body_dict)        
+        # find query values
+        split_query = parmalink.split('?')
+        if len(split_query)>1:
+            parmalink = split_query[0]
+            query = split_query[1].split('=') # ["id","chp"]
+
+        parmalinks = {'/hi','/echo','/user'}
+        if parmalink not in parmalinks: 
+            page_not_found(client_socket)
+
+        if parmalink=='/hi' and method=='GET':
+            get_hi(client_socket)
+        elif parmalink=='/echo' and method=='POST':
+            post_echo(client_socket,msg_body_dict)        
+        elif parmalink=='/user' and method=='POST':
+            post_user(client_socket,msg_body_dict)
+        elif parmalink=='/user' and method=='GET':
+            if query[1] not in users:
+                page_not_found(client_socket)
+            else:
+                get_user(client_socket,query)
+        
 
         # 클라이언트에게 응답
         # client_socket.sendall("welcome!".encode())
